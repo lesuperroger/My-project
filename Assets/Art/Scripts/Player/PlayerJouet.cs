ac42e4ddd27c33bcdda06fd27c9bde0195d8c6b4
Jean-LoupIsReal <131626426+Jean-LoupIsReal@users.noreply.github.com>
2024-03-16 08:17:21 -0400

    joueur fini

--- Assets/Art/Scripts/Player/PlayerJouet.cs
+++ Assets/Art/Scripts/Player/PlayerJouet.cs
@@ -6,29 +6,54 @@ using UnityEngine;
 public class PlayerJouet : MonoBehaviour
 {
     public int pv = 3;
-    public int jumpForce = 10;
+    public int jumpForce = 10; 
     public int speed = 10;
     public bool isOnGround = true;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
     private Rigidbody2D rb;
-
     private float move;
+    private bool isFacingRight;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        isFacingRight = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //movement gauche/droite
         move = Input.GetAxis("Horizontal");
-        float dirRotation = Convert.ToInt32(Input.GetKey(KeyCode.LeftArrow)) - Convert.ToInt32(Input.GetKey(KeyCode.RightArrow));
-
-
         rb.velocity = new Vector2(move * speed, rb.velocity.y);
+        //flip sprite
+        Flip();
+
+        Jump();
         
-        if (Input.GetKeyDown(KeyCode.Space) && isGrouded)
+    }
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
             rb.velocity = (jumpForce * transform.up);
+        if (Input.GetKeyDown(KeyCode.Space) && rb.velocity.y > 0f)
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+    private void Flip()
+    {
+        if (isFacingRight && move < 0f || !isFacingRight && move > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
     }
 }
