0fcc6b6cd8f58f72bbdf49edcd38c61eba04453d
Jean-LoupIsReal <bbla9394@gmail.com>
2024-03-18 12:41:58 -0400

    jeu finissable + couple of fixes

--- Assets/MobDm.cs
+++ Assets/MobDm.cs
@@ -12,7 +12,7 @@ public class MobDm : MonoBehaviour
         playerSoundManager = GetComponent<PlayerSoundManager>();
     }
 
-        private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
